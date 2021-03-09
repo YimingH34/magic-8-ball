d70088752c446e36316bb419ec1f4711b0f582c6
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-09 15:20:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,9 @@ input.onButtonPressed(Button.A, function () {
 })
 // When you shake something, it will set the outcome to 0,1,2 and it will act accordingly.
 input.onGesture(Gesture.Shake, function () {
+    // Make the code to choose either 0,1,2
     the_outcome = randint(0, 2)
+    // If the number is one then it will show yes, if the number is 1 then it will show no otherwise it will show maybe
     if (the_outcome == 2) {
         basic.showString("YES")
     } else if (the_outcome == 1) {
